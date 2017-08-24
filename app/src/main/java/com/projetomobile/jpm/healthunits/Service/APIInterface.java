19b12a8cf7ebfe1f5add51e08a572d8666d75739
phelipepalmeira <phelipematheus@gmail.com>
2017-08-23 21:10:26 -0300

    Utilizando Retrofit para fazer a requisição da API

---
+++ app/src/main/java/com/projetomobile/jpm/healthunits/Service/APIInterface.java
@@ -0,0 +1,23 @@
+package com.projetomobile.jpm.healthunits.Service;
+
+import com.projetomobile.jpm.healthunits.ValueObject.Estabelecimento;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by phelipepalmeira on 23/08/17.
+ */
+
+public interface APIInterface {
+
+    @GET("rest/estabelecimentos")
+    Call<List<Estabelecimento>> listEstabelecimento();
+
+    @GET("/rest/estabelecimentos/latitude/{latitude}/longitude/{longitude}/raio/{raio}")
+    Call<List<Estabelecimento>> listEstabelecimentoRaio(@Path("latitude") String lat,@Path("longitude") String lon,@Path("raio") String raio);
+
+}
--- app/src/main/java/com/projetomobile/jpm/healthunits/Telas/TelaSearchFilter.java
+++ app/src/main/java/com/projetomobile/jpm/healthunits/Telas/TelaSearchFilter.java
@@ -10,6 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.projetomobile.jpm.healthunits.R;
+import com.projetomobile.jpm.healthunits.Service.APIInterface;
+import com.projetomobile.jpm.healthunits.ValueObject.Estabelecimento;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
 
 public class TelaSearchFilter extends AppCompatActivity{
 
@@ -27,17 +33,26 @@ public class TelaSearchFilter extends AppCompatActivity{
 
         //Início da chamada de tela caso tenha
         this.chamaMaps();
+        // Testando se a API esta funcionando
+        chamadaAPI();
     }
 
     private void chamaMaps(){
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent callTelaMaps = new Intent(TelaSearchFilter.this,TelaMaps.class);
                 startActivity(callTelaMaps);
                 finish();
             }
         });
+    }
+
+    public List<Estabelecimento> chamadaAPI(){
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://mobile-aceite.tcu.gov.br/mapa-da-saude/").build();
+        APIInterface estabelecimentos = retrofit.create(APIInterface.class);
+        return (List<Estabelecimento>) estabelecimentos.listEstabelecimento();
     }
 
 }
